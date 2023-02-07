5d6c2b14036e2457f3959cb57723ef8657365bc7
slchld1 <jaecho203@gmail.com>
2023-02-07 15:10:48 -0500

    sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,16 @@
 // sequelize
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        prot: 3306,
+    }
+);
+
+module.exports = sequelize;
