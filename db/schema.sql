7ec9acc46b00ac27102be0fea6c0b10993cf24bc
Minjae Cho <113135811+slchld1@users.noreply.github.com>
2023-02-07 18:53:08 -0500

    seed database/example userData.json

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,3 @@
 -- create tables
+DROP DATABASE IF EXISTS id_userDB;
+CREATE DATABASE id_userDB;
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,1 +1,17 @@
 // seeds
+const sequelize = require('./config/connection');
+const { Customer } = require('../models');
+
+const userData = require('./userData.json');
+
+const seededDatabase = async () => {
+        await sequelize.sync({  force: true });
+
+        await Customer.bulkCreate(userData, {
+            individualHooks: true,
+            returning: true,
+        })
+        process.exit(0);
+}
+
+seededDatabase();
