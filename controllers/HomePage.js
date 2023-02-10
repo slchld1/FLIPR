1c125741559d986ea59b0072847b1b87245cd320
Minjae Cho <113135811+slchld1@users.noreply.github.com>
2023-02-09 20:51:35 -0500

    login rein

--- controllers/HomePage.js
+++ controllers/HomePage.js
@@ -1,6 +1,25 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const withAuth = require('../utils/auth');
+const { Customer } = require('../models');
 
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const userData = await Customer.findAll({
+            attributes: {exclude: ['password']},
+            order: [['name', 'ASC']],
+        });
+
+        const users = userData.map((project) => project.get({ plain: true }));
+
+        res.render('body', {
+            users,
+            logged_in: req.session.logged_in,
+        });
+    }   catch (err) {
+            res.status(500).json(err);
+    }
+})
 
 router.get('/join', async(req, res) => {
     res.render('join');
--- controllers/index.js
+++ controllers/index.js
@@ -2,26 +2,7 @@
 const router = require('express').Router()
 const apiRoutes = require('./api/index');
 const homeRoutes = require('./HomePage.js');
-const withAuth = require('../utils/auth');
-const { Customer } = require('../models');
 
-router.get('/', withAuth, async (req, res) => {
-    try {
-        const userData = await Customer.findAll({
-            attributes: {exclude: ['password']},
-            order: [['name', 'ASC']],
-        });
-
-        const users = userData.map((project) => project.get({ plain: true }));
-
-        res.render('body', {
-            users,
-            logged_in: req.session.logged_in,
-        });
-    }   catch (err) {
-            res.status(500).json(err);
-    }
-})
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
