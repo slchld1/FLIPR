c9134a1dda9fba9ec54803a91d59cf1548f863c4
Minjae Cho <113135811+slchld1@users.noreply.github.com>
2023-02-09 20:48:33 -0500

    logout

--- controllers/HomePage.js
+++ controllers/HomePage.js
@@ -1,26 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Customer } = require('../models');
-const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-        try {
-            const userData = await Customer.findAll({
-                attributes: {exclude: ['password']},
-                order: [['name', 'ASC']],
-            });
-    
-            const users = userData.map((project) => project.get({ plain: true }));
-    
-            res.render('main', {
-                users,
-                logged_in: req.session.logged_in,
-            });
-        }   catch (err) {
-                res.status(500).json(err);
-        }
-    res.render('body');
-});
 
 router.get('/join', async(req, res) => {
     res.render('join');
